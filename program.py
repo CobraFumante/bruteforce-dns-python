de5de4f474027bf6b268c9c0a5788343098decaa
Thomaz Edson Souza Gomes <thomazedsonphb@gmail.com>
2018-12-03 00:01:41 -0200

    Pequenas modificações
    Olá, é o cobra fumante da apda, obrigado pelo follow, propuz algumas mudanças para seu código! não esquece de testar pq eu não tive a possibilidade de testar o codigo assim. Vlw ae e pfv entra em contato comigo pelo face: https://www.facebook.com/profile.php?id=100008643454012 e pela steam: http://s.team/p/cnwc-btdv/HHTWHDTB

--- program.py
+++ program.py
@@ -1,9 +1,13 @@
+import sys
+
 from __methods__ import Methods
+
 class Program():
     def __init__(self):
         self.method = ""
         self.methods = ["1","2","3"]
         self.host = ""
+        
     def main(self):
         print('\n'
               '        ████▀░░░░░░░░░░░░░░░░░▀████ \n'
@@ -25,6 +29,7 @@ class Program():
         "[1] Reverse-DNS\n[2] Brute DNS with list default\n[3] DNS Brute-Force with a file\n>> ")
         if self.method not in self.methods:
             print("[!] Method not found!")
+            
     def run(self):
         if self.method == "1":
             dns = Methods()
@@ -39,13 +44,16 @@ class Program():
             dns.hostwordlist()
             dns.file()
             dns.dnsbywordlist()
+            
     def tag(self):
         while True:
-            self.tag = input(r"Do you wanna continue? [s\n]: ")
+            self.tag = input(r"Do you wanna continue? [s/n\n]: ") # não iria aparecer o n (apague esse comentario)
             if self.tag.lower() == "n":
-                return exit()
+                print('Bye Bye')
+                sys.exit() #creio que fica mais bonito assim (apague esse comentario)
             elif self.tag.lower() == "s":
-                return program.main, program.run()
+                self.main() #creio que fica mais bonito assim (apague esse comentario)
+                self.run() #creio que fica mais bonito assim (apague esse comentario)
             else:
                 print("Error.. We not has this option here")
 
